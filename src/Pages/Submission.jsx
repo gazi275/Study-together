77b8bcb399ce716cc8362916f7f6ec4f9dd69df4
gazi275 <gaziazad270@gmail.com>
2023-11-09 09:09:57 +0600

    submission page is updated

--- src/Pages/Submission.jsx
+++ src/Pages/Submission.jsx
@@ -1,10 +1,20 @@
 
 
 const Submission = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        
+       
+         const file = e.target.file.value;
+        const email = e.target.email.value;
+        console.log(file,email);
+    }
+
     return (
         <div>
           
-<link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet"/>
+
 <div className=" min-h-screen flex  justify-center  py-12 px-4 sm:px-6 lg:px-8 bg-gray-500 bg-no-repeat bg-cover relative items-center"
 	style={{ backgroundImage: 'url("https://i.ibb.co/tBvVpMm/images-1.jpg")' }}>
 	<div className="absolute bg-black opacity-60 inset-0 z-0"></div>
@@ -15,17 +25,17 @@ const Submission = () => {
 			</h2>
 			<p className="mt-2 text-sm text-gray-400">Lorem ipsum is placeholder text.</p>
 		</div>
-        <form className="mt-8 space-y-3" action="#" method="POST">
+        <form onSubmit={handleSubmit} className="mt-8 space-y-3" action="#" method="POST">
                     <div className="grid grid-cols-1 space-y-2">
-                        <label className="text-sm font-bold text-gray-500 tracking-wide">Title</label>
-                            <input className="text-base p-2 border border-gray-300 rounded-lg focus:outline-none focus:border-indigo-500" type="" placeholder="mail@gmail.com"/>
+                        <label className="text-sm font-bold text-gray-500 tracking-wide">Email</label>
+                            <input className="text-base p-2 border border-gray-300 rounded-lg focus:outline-none focus:border-indigo-500" type="email" name="email" placeholder="mail@gmail.com"  required=""/>  />
                     </div>
                     <div className="grid grid-cols-1 space-y-2">
                                     <label className="text-sm font-bold text-gray-500 tracking-wide">Attach Document</label>
                         <div className="flex items-center justify-center w-full">
                             <label className="flex flex-col rounded-lg border-4 border-dashed w-full h-60 p-10 group text-center">
                                 <div className="h-full w-full text-center flex flex-col  justify-center items-center  ">
-                                    <svg xmlns="http://www.w3.org/2000/svg" className="w-10 h-10 text-blue-400 group-hover:text-blue-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className=" text-blue-400 group-hover:text-blue-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />
                                     </svg>
                                     <div className="flex flex-auto max-h-48 w-2/5 mx-auto -mt-10">
@@ -33,10 +43,13 @@ const Submission = () => {
                                     </div>
                                 
                                     <div className="w-full">
-                  <label  className="block mb-2 text-sm font-bold text-gray-900 dark:text-white">Email</label>
-                  <input type="file" name="email" id="brand" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Enter your Email" required=""/>
+                  <label  className="block mb-2 text-sm font-bold text-gray-900 dark:text-white">Attched a File</label>
+                  <input type="file" name="file" id="brand" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Enter your Email" required=""/>
                   </div>
-                               
+                  <div className="sm:col-span-2">
+                  <label for="description" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
+                  <textarea id="description" rows="3" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Your description here"></textarea>
+              </div>       
                                
                                
                                 </div>
@@ -46,7 +59,7 @@ const Submission = () => {
                         </div>
                     </div>
                             <p className="text-sm text-gray-300">
-                                <span>File type: doc,pdf,types of images</span>
+                                <span>File type: pdf</span>
                             </p>
                     <div>
                         <button type="submit" className="my-5 w-full flex justify-center bg-blue-500 text-gray-100 p-4  rounded-full tracking-wide
